f7c17853d3139ff843176b5bcf3b39b30aab772b
Kevin Gilbert <pompeii@vulcan.(none)>
2014-10-28 21:19:14 -0500

    Cleaned up spacing in testbench

--- sim/testbench.v
+++ sim/testbench.v
@@ -24,63 +24,63 @@ module testbench(CLK, led0, led1, led2, led3);
     input CLK;
     output wire led0, led1, led2, led3;
     
-	reg can_clk = 0;
-	reg uart_clk = 0;
-	parameter RUN_LEN = 120;	
+    reg can_clk = 0;
+    reg uart_clk = 0;
+    parameter RUN_LEN = 120;    
 
-	initial begin
-		can_clk = 0;
-	end
+    initial begin
+        can_clk = 0;
+    end
 
     wire can_lo_out_0, can_hi_out_0;
-	wire can_lo_in_0, can_hi_in_0;
+    wire can_lo_in_0, can_hi_in_0;
     wire can_lo_out_1, can_hi_out_1;
-	wire can_lo_in_1, can_hi_in_1;
+    wire can_lo_in_1, can_hi_in_1;
 
     wire can_hi, can_lo;
 
 `ifndef SINGLE_NODE
-	//assign can_lo_in_0 = can_lo_out_1;
-	//assign can_hi_in_0 = can_hi_out_1;
-	//assign can_lo_in_1 = can_lo_out_0;
-	//assign can_hi_in_1 = can_lo_out_0;
+    //assign can_lo_in_0 = can_lo_out_1;
+    //assign can_hi_in_0 = can_hi_out_1;
+    //assign can_lo_in_1 = can_lo_out_0;
+    //assign can_hi_in_1 = can_lo_out_0;
     assign can_hi = can_hi_out_0 | can_hi_out_1;
     //assign can_lo = can_lo_out_0 | can_lo_out_1;
     assign can_lo = ~can_hi;
 `else
-	assign can_lo_in_0 = can_lo_out_0;
-	assign can_hi_in_0 = can_hi_out_0;
+    assign can_lo_in_0 = can_lo_out_0;
+    assign can_hi_in_0 = can_hi_out_0;
 `endif
     custom_can_node can0(   can_clk, 
-							can_clk, 
-							0, 
-							can_lo, 
-							can_lo_out_0, 
-							can_hi, 
-							can_hi_out_0, 
-							led0, 
-							led1, 
-							4'h0 
-						);
+                            can_clk, 
+                            0, 
+                            can_lo, 
+                            can_lo_out_0, 
+                            can_hi, 
+                            can_hi_out_0, 
+                            led0, 
+                            led1, 
+                            4'h0 
+                        );
 `ifndef SINGLE_NODE
-	custom_can_node can1(	can_clk, 
-							can_clk, 
-							0, 
-							can_lo, 
-							can_lo_out_1, 
-							can_hi, 
-							can_hi_out_1, 
-							led2, 
-							led3, 
-							4'h1
-						);
+    custom_can_node can1(    can_clk, 
+                            can_clk, 
+                            0, 
+                            can_lo, 
+                            can_lo_out_1, 
+                            can_hi, 
+                            can_hi_out_1, 
+                            led2, 
+                            led3, 
+                            4'h1
+                        );
 `endif
 
-	integer i=0;
-	always@(can_clk) begin
-		can_clk <= #10 ~can_clk;
-		i = i+1;
-		if(i>RUN_LEN) 
-			$finish;
-	end
+    integer i=0;
+    always@(can_clk) begin
+        can_clk <= #10 ~can_clk;
+        i = i+1;
+        if(i>RUN_LEN) 
+            $finish;
+    end
 endmodule
