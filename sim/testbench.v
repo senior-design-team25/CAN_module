d7b0da8c1cdf2a80c14d389ae81c4f1a4bd96e33
Kevin Gilbert <pompeii@vulcan.(none)>
2014-10-28 21:20:17 -0500

    Cleaned up artifacts from testbench

--- sim/testbench.v
+++ sim/testbench.v
@@ -40,12 +40,7 @@ module testbench(CLK, led0, led1, led2, led3);
     wire can_hi, can_lo;
 
 `ifndef SINGLE_NODE
-    //assign can_lo_in_0 = can_lo_out_1;
-    //assign can_hi_in_0 = can_hi_out_1;
-    //assign can_lo_in_1 = can_lo_out_0;
-    //assign can_hi_in_1 = can_lo_out_0;
     assign can_hi = can_hi_out_0 | can_hi_out_1;
-    //assign can_lo = can_lo_out_0 | can_lo_out_1;
     assign can_lo = ~can_hi;
 `else
     assign can_lo_in_0 = can_lo_out_0;
@@ -53,7 +48,7 @@ module testbench(CLK, led0, led1, led2, led3);
 `endif
     custom_can_node can0(   can_clk, 
                             can_clk, 
-                            0, 
+                            1'b0, 
                             can_lo, 
                             can_lo_out_0, 
                             can_hi, 
@@ -63,9 +58,9 @@ module testbench(CLK, led0, led1, led2, led3);
                             4'h0 
                         );
 `ifndef SINGLE_NODE
-    custom_can_node can1(    can_clk, 
+    custom_can_node can1(   can_clk, 
                             can_clk, 
-                            0, 
+                            1'b0, 
                             can_lo, 
                             can_lo_out_1, 
                             can_hi, 
