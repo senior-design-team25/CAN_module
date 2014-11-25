5b75ece3b4a8c2204a39591332ae80ef5db4dc9a
Kevin Gilbert <k.gilbert.ch@gmail.com>
2014-11-25 00:53:16 -0600

    Moved Chris's updated UART module into sim directory to test

--- sim/uart.v
+++ sim/uart.v
@@ -2,70 +2,80 @@
 // UART interface
 //
 
-module uarttx(data, send, ready, tx, clk);
+module uarttx(
+    clock115200, resetn, data, send, ready,
+    UART_TX
+);
 
-//parameter DIV;
-
+input clock115200;
+input resetn;
 input [7:0] data;
 input send;
-output ready;
-output reg tx;
-input clk;
+output reg ready;
+output UART_TX = tx;
 
-wire fsend;
-reg [3:0] state = 0;
+reg tx;
+reg [3:0] state;
 
-assign ready = state == 0;
-
-//flag f(send, fsend, state == 1);
-
-always @(posedge clk) begin
-    if (state == 0) begin
+always @(posedge clock115200 or negedge resetn) begin
+    if (!resetn) begin
         tx <= 1'b1;
-        state <= send ? 1 : 0;
+        ready <= 0;
+        state <= 0;
     end else begin
-        tx <= state == 1 ? 1'b0 : data[state-2];
-        state <= state == 9 ? 0 : state + 1;
+        if (state == 0) begin
+            tx <= 1'b1;
+            ready <= send ? 1'b0 : 1'b1;
+            state <= send ? 4'h1 : 4'h0;
+        end else begin
+            tx <= state == 4'h1 ? 1'b0 : data[state-2];
+            state <= state == 4'h9 ? 4'h0 : state + 1'b1;
+        end
     end
 end
 
 endmodule
 
-/*
-module uartrx(data, recv, rx, clk);
 
-parameter DIV;
+module uartrx(
+    clock115200, resetn, data, recv,
+    UART_RX
+);
 
+input clock115200;
+input resetn;
 output reg [7:0] data;
-output recv;
-input rx;
-input clk;
+output reg recv;
+input UART_RX;
 
-wire uclk;
-reg [4:0] state = 0;
+wire rx = UART_RX;
+reg [3:0] state;
 reg [7:0] buffer;
 
-div #(DIV/2) (clk, uclk);
-pulse (state == 0, recv);
-
-always @(posedge uclk) begin
-    if (state == 18) begin
-        data <= buffer;
+always @(posedge clock115200 or negedge resetn) begin
+    if (!resetn) begin
+        data <= 0;
+        recv <= 0;
         state <= 0;
-    end else if (state == 0) begin
-        state <= rx == 1'b0 ? 1 : 0;
-    end else if (state[0] == 1'b1) begin
-        state <= state + 1;
     end else begin
-        buffer[state[4:1]] <= rx;
-        state <= state + 1;
-    end 
+        if (state == 0) begin
+            recv <= 1'b0;
+            state <= rx == 1'b0 ? 4'h1 : 4'h0;
+        end else if (state == 9) begin
+            data <= buffer;
+            recv <= 1'b1;
+            state <= 4'h0;
+        end else begin
+            buffer[state-1] <= rx;
+            state <= state + 1'b1;
+        end
+    end
 end
 
 endmodule
 
 
-
+/*
 module uart50tx(data, send, ready, tx, clk);
 
 input [7:0] data;
